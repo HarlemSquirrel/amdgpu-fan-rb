01e3f26f6527b103adf39c00b8832901c62a03e5
Kevin McCormack <harlemsquirrel@gmail.com>
2019-01-14 17:25:52 -0500

    Add AmdgpuService

--- lib/amdgpu_fan_cli.rb
+++ lib/amdgpu_fan_cli.rb
@@ -1,29 +1,21 @@
 # frozen_string_literal: true
 
-# require 'thor'
 require_relative '../config/environment'
 
 # The main class
 class AmdgpuFanCli < Thor
-  FAN_POWER_FILE = Dir.glob("/sys/class/drm/card0/device/**/pwm1").first
-  FAN_MAX_POWER_FILE = Dir.glob("/sys/class/drm/card0/device/**/pwm1_max").first
-  FAN_INPUT_FILE = Dir.glob("/sys/class/drm/card0/device/**/fan1_input").first
-  # FAN_MODES = { auto: '2', manual: '1' }.freeze
-  POWER_MAX_FILE = '/sys/class/drm/card0/device/hwmon/hwmon2/power1_cap'
-  POWER_AVG_FILE = '/sys/class/drm/card0/device/hwmon/hwmon2/power1_average'
-
   desc 'auto', 'Set mode to automatic (requires sudo)'
   def auto
-    set_mode(:auto)
+    amdgpu_service.set_fan_mode! :auto
+    puts fan_status
   end
 
   desc 'set PERCENTAGE', 'Set fan speed to PERCENTAGE (requires sudo)'
   def set(percentage)
     return puts "Invalid percentage" unless (0..100).cover?(percentage.to_i)
 
-    set_mode(:manual) unless amdgpu_service.fan_mode == 'manual'
-    puts "Setting fan to #{setting_from_percent percentage}/#{max}..."
-    set_manual_speed setting_from_percent(percentage)
+    amdgpu_service.set_fan_manual_speed! percent: percentage
+    puts fan_status
   end
 
   desc 'status', 'View device info, current fan speed, and temperature'
@@ -31,11 +23,10 @@ class AmdgpuFanCli < Thor
     print_radeon_logo
     puts "📺\tGPU:   #{amdgpu_service.name}",
          "📄\tvBIOS: #{amdgpu_service.vbios_version}",
-         "🌀\tFan:   #{amdgpu_service.fan_mode} mode running at " \
-          "#{current_percentage.round}% ~ #{rpm} rpm",
+         fan_status,
          "🌡\tTemp:  #{amdgpu_service.temperature}°C",
          "⚡\tPower: #{amdgpu_service.power_dpm_state} mode using " \
-          "#{current_power} / #{power_max} Watts",
+          "#{amdgpu_service.power_draw} / #{amdgpu_service.power_max} Watts",
          "⚖\tLoad: #{amdgpu_service.busy_percent}%"
   end
 
@@ -54,9 +45,9 @@ class AmdgpuFanCli < Thor
 
     loop do
       puts "#{Time.now.strftime("%F %T")} " \
-           "Fan: #{rpm} rpm (#{current_percentage.round}%), " \
+           "Fan: #{amdgpu_service.fan_speed_rpm} rpm (#{amdgpu_service.fan_speed_percent}%), " \
            "Load: #{amdgpu_service.busy_percent}%, " \
-           "Power: #{current_power} W, " \
+           "Power: #{amdgpu_service.power_draw} W, " \
            "Temp: #{amdgpu_service.temperature}°C "
       sleep seconds.to_i
     end
@@ -68,56 +59,16 @@ class AmdgpuFanCli < Thor
     @amdgpu_service ||= AmdgpuService.new
   end
 
-  def current
-    File.read FAN_POWER_FILE
-  end
-
-  def current_percentage
-    current.to_f / max.to_i * 100
-  end
-
-  def current_power
-    (File.read(POWER_AVG_FILE).strip.to_f / 1000000).round(2)
-  end
-
   def current_time
     Time.now.strftime("%F %T")
   end
 
-  def max
-    @max ||= File.read(FAN_MAX_POWER_FILE).to_i
-  end
-
-  # def in_auto_mode?
-  #   File.read(FAN_MODE_FILE).strip == '2'
-  # end
-
-  # def in_manual_mode?
-  #   File.read(FAN_MODE_FILE).strip == '1'
-  # end
-
-  def power_max
-    @power_max ||= (File.read(POWER_MAX_FILE).strip.to_f / 1000000).round(2)
+  def fan_status
+    "🌀\tFan:   #{amdgpu_service.fan_mode} mode running at " \
+     "#{amdgpu_service.fan_speed_percent}% ~ #{amdgpu_service.fan_speed_rpm} rpm"
   end
 
   def print_radeon_logo
     puts File.read('lib/radeon_r_black_red_100x100.ascii')
-  end
-
-  def rpm
-    File.read(FAN_INPUT_FILE).strip
-  end
-
-  # def set_mode(mode)
-  #   puts "Setting mode to #{mode}"
-  #   `echo "#{FAN_MODES[mode]}" | sudo tee #{FAN_MODE_FILE}`
-  # end
-
-  def set_manual_speed(speed)
-    `echo "#{speed}" | sudo tee #{FAN_POWER_FILE}`
-  end
-
-  def setting_from_percent(percent)
-    (percent.to_f / 100 * max.to_i).round
   end
 end
--- lib/amdgpu_service.rb
+++ lib/amdgpu_service.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
+## AmdgpuService
+#
+# A service class for reading and interacting with AMD radeon graphics cards
+# through the amdgpu Linux kernel driver.
 class AmdgpuService
+  BASE_FOLDER = '/sys/class/drm'
   FAN_MODES = { '1' => 'manual', '2' => 'auto' }.freeze
 
   attr_reader :card_num
@@ -10,11 +15,27 @@ class AmdgpuService
   end
 
   def busy_percent
-    File.read("/sys/class/drm/card#{card_num}/device/gpu_busy_percent").strip
+    File.read("#{base_card_folder}/gpu_busy_percent").strip
   end
 
   def fan_mode
     FAN_MODES[File.read(fan_mode_file).strip] || 'unknown'
+  end
+
+  def set_fan_mode!(mode)
+    `echo "#{FAN_MODES.key(mode.to_s)}" | sudo tee #{fan_mode_file}`
+  end
+
+  def fan_speed_percent
+    (fan_speed_raw.to_f / fan_speed_raw_max.to_i * 100).round
+  end
+
+  def fan_speed_raw_max
+    @fan_speed_raw_max ||= File.read("#{fan_power_file}_max").strip
+  end
+
+  def fan_speed_rpm
+    File.read(fan_input_file).strip
   end
 
   def name
@@ -22,12 +43,29 @@ class AmdgpuService
   end
 
   def power_dpm_state
-    File.read("/sys/class/drm/card#{card_num}/device/power_dpm_state").strip
+    File.read("#{base_card_folder}/power_dpm_state").strip
   end
 
-  def set_mode!(mode)
-    puts "Setting mode to #{mode}"
-    `echo "#{FAN_MODES.key(mode.to_s)}" | sudo tee #{fan_mode_file}`
+  def power_draw
+    power_raw_to_watts File.read(power_avg_file)
+  end
+
+  def power_max
+    @power_max ||= power_raw_to_watts File.read(power_max_file)
+  end
+
+  def set_fan_manual_speed!(percent: nil, raw: nil)
+    if valid_fan_percent_speed?(percent)
+      new_raw = (percent.to_f / 100 * fan_speed_raw_max.to_i).round
+    elsif valid_fan_raw_speed?(raw)
+      new_raw = raw
+    end
+
+    raise(::Error, 'Invalid fan speed provided') unless defined?(new_raw)
+
+    set_fan_mode!(:manual) unless fan_mode == 'manual'
+
+    `echo "#{new_raw}" | sudo tee #{fan_power_file}`
   end
 
   def temperature
@@ -35,24 +73,62 @@ class AmdgpuService
   end
 
   def vbios_version
-    File.read("/sys/class/drm/card#{card_num}/device/vbios_version").strip
+    @vbios_version ||= File.read("#{base_card_folder}/vbios_version").strip
   end
 
   private
 
+  def base_card_folder
+    @base_card_folder ||= "#{BASE_FOLDER}/card#{card_num}/device"
+  end
+
+  def fan_input_file
+    @fan_input_file ||= Dir.glob("#{base_card_folder}/**/fan1_input").first
+  end
+
   def fan_mode_file
-    Dir.glob("/sys/class/drm/card#{card_num}/device/**/pwm1_enable").first
+    @fan_mode_file ||= Dir.glob("#{base_card_folder}/**/pwm1_enable").first
+  end
+
+  def fan_power_file
+    @fan_power_file ||= Dir.glob("#{base_card_folder}/**/pwm1").first
+  end
+
+  def fan_speed_raw
+    File.read(fan_power_file).strip
   end
 
   def gpu_pci_id
-    `lspci -v | grep VGA`.split(' ').first
+    @gpu_pci_id ||= `lspci -v | grep VGA`.split(' ').first
   end
 
   def lspci_subsystem
-    `lspci -v -s #{gpu_pci_id} | grep "Subsystem:"`
+    @lspci_subsystem ||= `lspci -v -s #{gpu_pci_id} | grep "Subsystem:"`
+  end
+
+  def power_avg_file
+    @power_avg_file ||= Dir.glob("#{base_card_folder}/**/power1_average").first
+  end
+
+  def power_max_file
+    @power_avg_file ||= Dir.glob("#{base_card_folder}/**/power1_cap").first
+  end
+
+  def power_raw_to_watts(raw_string)
+    (raw_string.strip.to_f / 1_000_000).round(2)
   end
 
   def temperature_file
-    Dir.glob("/sys/class/drm/card#{card_num}/device/**/temp1_input").first
+    @temperature_file ||= Dir.glob("#{base_card_folder}/**/temp1_input").first
   end
+
+  def valid_fan_raw_speed?(raw)
+    (1..fan_speed_raw_max.to_i).include?(raw.to_i)
+  end
+
+  def valid_fan_percent_speed?(percent)
+    (1..100.to_i).include?(percent.to_i)
+  end
+
+  class Error < StandardError; end
 end
