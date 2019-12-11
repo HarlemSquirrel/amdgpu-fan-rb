25d1dcc783b541cf33cfe020e3e8c4b65fe7a53d
Kevin McCormack <harlemsquirrel@gmail.com>
2019-12-10 22:39:18 -0500

    Sort connectors

--- lib/amdgpu_fan/connector.rb
+++ lib/amdgpu_fan/connector.rb
@@ -8,8 +8,11 @@ module AmdgpuFan
     attr_reader :card_num, :dir_path, :index, :type
 
     class << self
+      ##
+      # Return an array of connector objects for the provided card number.
+      # The files are sorted to improve how they are displayed to the user.
       def where(card_num:)
-        Dir["/sys/class/drm/card#{card_num}/card#{card_num}-*"].map do |dir_path|
+        Dir["/sys/class/drm/card#{card_num}/card#{card_num}-*"].sort.map do |dir_path|
           Connector.new card_num: card_num,
                         dir_path: dir_path,
                         index: dir_path[-1],
